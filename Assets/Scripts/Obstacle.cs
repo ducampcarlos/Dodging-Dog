f59d1e2d99e4f8cbf4a10b50dee40ae8d4a4535d
ducampcarlos <ducampcarlos@gmail.com>
2025-04-01 09:32:49 -0300

    Added: Saw as falling obstacle.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
